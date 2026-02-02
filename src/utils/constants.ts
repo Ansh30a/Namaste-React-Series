8ec4103fc571150f49ed30fbad2f58b5cd539f30
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-02 18:35:46 +0530

    feat: links

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+// Named export
+// string we have used coz we have to mention what will be the type of our link o/w ts wil give error.
+
+export const CDN_URL:string = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const LOGO_URL:string = "https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY="
+
+// 2 types of export are there :
+        //   - Default export
+        //   - Named export
+    
