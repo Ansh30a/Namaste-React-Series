a3761768152ae935cf4ec48ba4415f8ecf4df759
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-04 00:27:43 +0530

    feat: Add Shimmer to the restaurant cards

---
+++ src/components/Shimmer.tsx
@@ -0,0 +1,12 @@
+const Shimmer = () => {
+    return (
+        <div className="shimmer-conatiner">
+            <div className="shimmer-cards">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+        </div>
+    );
+};
+
+export default Shimmer;
