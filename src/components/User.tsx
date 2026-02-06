911e380aa33f614b0b8cbd64acb8936435d2648f
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-06 23:10:00 +0530

    feat(ui): user information

---
+++ src/components/User.tsx
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-card">
+            <h2>Name: Shreya</h2>
+            <h3>Location: New Delhi</h3>
+            <h4>Contact info : @chopra_shreya03</h4>
+        </div>
+    );
+};
+
+export default User;
