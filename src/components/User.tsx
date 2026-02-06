5dca017a83d61750fabb463e680574a0091ba0e1
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-07 01:40:02 +0530

    update: functional component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,8 +1,17 @@
-const User = () => {
+import { useState } from "react";
+ 
+type UserProps = {
+    name: string;
+}
+ 
+
+const User= ({name}: UserProps) => {
+    const [count] =  useState<any>(0);
+
     return (
         <div className="user-card">
-            <h2>Name: Shreya</h2>
-            <h3>Location: New Delhi</h3>
+            <h2>Name: {name}</h2>
+            <h3>Location: New delhi</h3>
             <h4>Contact info : @chopra_shreya03</h4>
         </div>
     );
