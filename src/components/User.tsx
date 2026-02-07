dd6d30fe446798f862b64fbf1dc8f9d54664f4b6
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-07 23:52:26 +0530

    update: learn about unmounting phase of useEffect

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
  
 type UserProps = {
     name: string;
 }
  
-
 const User= ({name}: UserProps) => {
     const [count, setCount] =  useState<number>(0);
-    // const [count2] =  useState<any>(0);
+
+    useEffect(() => {
+        // API CALL
+        const timer = setInterval(() => {
+            console.log("namaste react op");
+        }, 1000);
+        console.log("useEffect");
+
+        // this is the unmounting phase of useEffect 
+        return () => {
+            clearInterval(timer);
+            console.log("useEffect Return");
+        };
+    }, []);
+
+    console.log("render");
 
     return (
         <div className="user-card">
             <h2>Name: {name}</h2>
             <button
-            onClick={() => {
-                setCount(count + 1);
+            onClick={() => {setCount(count + 1);
             }}
             >Count Updater</button>
             <h2>Count: {count}</h2>
-            {/* <h2>Count: {count2}</h2> */}
             <h3>Location: New delhi</h3>
             <h4>Contact info : @chopra_shreya03</h4>
         </div>
