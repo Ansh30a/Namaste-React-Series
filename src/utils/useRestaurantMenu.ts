4f77a47fd93f67fbc3f96f476e1d7de094359c76
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-08 03:32:28 +0530

    feat: fetched the menu api

---
+++ src/utils/useRestaurantMenu.ts
@@ -0,0 +1,19 @@
+import { MENU_API } from "../utils/constants";
+import { useEffect, useState } from "react";
+
+
+const useRestaurantMenu = (resId?: string) => {
+    const [resInfo, setResInfo] = useState<any>(null)
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(MENU_API + resId);
+        const json = await data.json();
+    }
+    return resInfo;
+
+}; 
+export default useRestaurantMenu;
