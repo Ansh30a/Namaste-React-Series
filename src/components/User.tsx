cc7d3c3e5204094cb5a19c89b6c66ddb304125d5
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-07 14:56:16 +0530

    Update: add the count increase button

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -6,11 +6,19 @@ type UserProps = {
  
 
 const User= ({name}: UserProps) => {
-    const [count] =  useState<any>(0);
+    const [count, setCount] =  useState<number>(0);
+    // const [count2] =  useState<any>(0);
 
     return (
         <div className="user-card">
             <h2>Name: {name}</h2>
+            <button
+            onClick={() => {
+                setCount(count + 1);
+            }}
+            >Count Updater</button>
+            <h2>Count: {count}</h2>
+            {/* <h2>Count: {count2}</h2> */}
             <h3>Location: New delhi</h3>
             <h4>Contact info : @chopra_shreya03</h4>
         </div>
