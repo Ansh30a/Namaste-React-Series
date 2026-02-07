55437c5fb9c115d31d964f331b915c4c021f350b
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-07 16:03:30 +0530

    update: learn about the class based components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,35 @@
 import User from "./User";
+import UserClass from "./UserClass";
 
-const About = () => {
+
+const About  = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series</h2>
-            <User />
+            <User name={"Shreya Chopra (function)"} />
+
+            <UserClass name={"First"} location={"Dehradun (class)"}/>
+            <UserClass name={"Second"} location={"US"}/>
         </div>
     );
 };
+
+
+/*
+React tries to batch up the task because manipulating DOM is really very expensive
+- Parent Constrcutor
+- Parent render
+      - First Constructor
+      - First Render
+
+      - Second Constuctor
+      - second Render
+
+- Child 1 component did mount
+- Child 2 component did mount
+- Parent component did mount
+
+*/
 
 export default About;
