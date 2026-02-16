de3b81c234e0fcc4581a06454a3461695b875b83
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-16 15:14:13 +0530

    cart slice

--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -10,14 +10,11 @@ export interface CartState {
 const initialState: CartState = {
     items: [],
 }
-
+ 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    // {
-        // These items are cart-items & initially it's empty
-        // items: [],
-    // },
+    
     // Reducer will always take 2 arguments: state & action (vvimp)
     //Reducers are basically objects that define what type of actions they can take.
     // action.payload is the daata that we send during dispatch (action).
